eca4146bf0acbb44b08ab34b1fc679f231a7238c
noor <noordragon2004@gmail.com>
2024-04-09 21:25:43 -0700

    predebloy

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 const { auth } = NextAuth(authConfig); // we do not want to use the
 import { publicRoutes, apiAuthPrefix, authRoutes, defaultLoginRedirect } from "./routes";
-export default auth((req)=>{
+//@ts-ignore
+export default auth((req:any)=>{
   //this is a middleware when we want to apply some code on requests to URL that matches the matcher in the config
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
